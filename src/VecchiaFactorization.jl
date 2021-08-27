65bbde40e0c7f60c1702793523ff89093bd66925
Ethan Anderes <anderes@ucdavis.edu>
2021-08-26 23:12:04 -0700

    first commit, basic mult seems working

--- src/VecchiaFactorization.jl
+++ src/VecchiaFactorization.jl
@@ -1,5 +1,137 @@
 module VecchiaFactorization
 
-# Write your package code here.
+using LinearAlgebra # BLAS.set_num_threads(1)
+using BlockArrays: PseudoBlockArray, blocks
+using BlockBandedMatrices: BlockDiagonal, BlockBidiagonal
+using FillArrays
+## using ArrayLayouts
+
+export Vecchia, InvVecchia
+
+# Make a Vecchia Struct
+# ===============================
+
+## represents cov Σ = invR * M * invR'
+struct Vecchia{T<:Number, RT<:AbstractMatrix{T}, MT<:AbstractMatrix{T}} <: Factorization{T}
+    R::Vector{RT}
+    M::Vector{MT}
+    bsds::Vector{Int} #blocksides
+end
+
+## represents inverse cov invΣ = R' * invM * R
+struct InvVecchia{T<:Number, RT<:AbstractMatrix{T}, MT<:AbstractMatrix{T}} <: Factorization{T}
+    R::Vector{RT}
+    invM::Vector{MT}
+    bsds::Vector{Int} # blocksides
+end
+
+InvVecc_or_Vecc{T} = Union{Vecchia{T}, InvVecchia{T}} where {T}
+
+## length(bsds) = nblocks
+## Block i has size bsds[i] × bsds[i]
+## length(R) = length(M) - 1
+
+# left mult 
+# ============================
+
+Base.:*(V::InvVecc_or_Vecc, w::AbstractVector) = lmul!(V, copy(w))
+
+## Σ = invR * M * invR'
+function LinearAlgebra.lmul!(V::Vecchia, w::AbstractVector)
+	wbB = blocks(PseudoBlockArray(w, V.bsds))
+	nb  = length(V.bsds)
+	## z = inv(V.R)' * w
+	for i in nb-1:-1:1
+		mul!(wbB[i], V.R[i]', wbB[i+1], -1, true)		
+	end
+	## q = V.M * z
+	for i in 1:nb	
+		mul!(wbB[i], V.M[i], copy(wbB[i]))
+	end
+	## inv(V.R) * q
+	for i in 1:nb-1
+		mul!(wbB[i+1], V.R[i], wbB[i], -1, true)		
+	end
+	return w
+end
+
+## invΣ = R' * invM * R
+function LinearAlgebra.lmul!(iV::InvVecchia, w::AbstractVector)
+	wbB = blocks(PseudoBlockArray(w, iV.bsds))
+	nb  = length(iV.bsds)
+	## z = iV.R * w
+	for i in nb-1:-1:1
+		mul!(wbB[i+1], iV.R[i], wbB[i], true, true)	
+	end
+	## q = iV.invM * z
+	for i in 1:nb	
+		mul!(wbB[i], iV.invM[i], copy(wbB[i]))
+	end
+	## iV.R' * q
+	for i in 1:nb-1
+		mul!(wbB[i], iV.R[i]', wbB[i+1], true, true)		
+	end
+	return w
+end
+
+# other LinearAlgebra methods
+# ====================================
+
+function Rmat(V::InvVecc_or_Vecc{T}) where {T}
+	nb  = length(V.bsds)
+	👀  = map(b->Matrix(Eye{T}(b)), V.bsds)
+	BlockBidiagonal(👀, V.R, :L)
+end
+
+function Rᴴmat(V::InvVecc_or_Vecc{T}) where {T}
+	nb  = length(V.bsds)
+	👀  = map(b->Matrix(Eye{T}(b)), V.bsds)
+	BlockBidiagonal(👀, map(x->Matrix(x'), V.R), :U)
+end
+
+# pinv(V)  and inv(V)
+# ----------------------------
+
+LinearAlgebra.pinv(V::Vecchia) = InvVecchia(deepcopy(V.R), map(pinv, V.M), V.bsds) 
+
+LinearAlgebra.pinv(V::InvVecchia) = Vecchia(deepcopy(V.R), map(pinv, V.M), V.bsds) 
+
+Base.inv(V::InvVecc_or_Vecc) = pinv(V)
+
+# size 
+# ------------------------------
+
+Base.size(V::InvVecc_or_Vecc{T}) where {T} = (nrws = sum(V.bsds); (nrws,nrws))
+
+Base.size(V::InvVecc_or_Vecc{T}, d) where {T} = d::Integer <= 2 ? size(V)[d] : 1
+
+# Matrix(V) 
+# ----------------------------
+
+# invΣ = R' * invM * R
+function Base.Matrix(iV::InvVecchia{T}) where {T}
+	invM = BlockDiagonal(iV.invM)
+	R    = Rmat(iV)
+	R' * invM * R
+end
+
+# Σ = invR * M * invR'
+function Base.Matrix(V::Vecchia)
+	M    = BlockDiagonal(V.M)
+	invR = inv(Rmat(V))
+	invR  * M * invR'
+end
+
+# inv_cholesky gives inv(cholesky(Σ)) -> LowerTriangular
+# ----------------------------
+
+function inv_cholesky(V::Vecchia)
+	L⁻¹s = map(cholesky(Hermitian(V.M))) do M 
+		## inv(cholesky(Hermitian(M, :L)).L)
+		Matrix(inv(cholesky(Hermitian(M, :L)).L))
+	end 
+	## TODO try and use skyline block matrix for 
+	BlockDiagonal(L⁻¹s) * Rmat(V)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,97 @@
+using LinearAlgebra
+BLAS.set_num_threads(1)
 using VecchiaFactorization
+using BlockArrays
 using Test
 
 @testset "VecchiaFactorization.jl" begin
     # Write your tests here.
+
+    K(x,y,θ) = exp(- θ * abs(x - y) ^ 0.8 )
+
+    # data parameters
+    θtru    = 1.5
+    nblocks, blocksz = 6, 150
+    bsn    = fill(blocksz, nblocks) 
+    n      = sum(bsn)
+    x      = sort(vcat(range(0,4,length=n-100), 4*rand(100)))
+    xb     = blocks(PseudoBlockArray(x, bsn))
+    Σ₀     = [ K.(xb[i],   xb[i]', θtru) for i = 1:nblocks ]
+    Σ₋₁    = [ K.(xb[i+1], xb[i]', θtru) for i = 1:nblocks-1 ]
+    R      = [ - (Σ₋₁[i] / Σ₀[i]) for i = 1:nblocks-1 ]
+    tailM₀ = [ Σ₀[i+1] + R[i] * Σ₋₁[i]' for i = 1:nblocks-1 ]
+    M      = [ Σ₀[1], tailM₀... ]
+
+    # Σ     = BlockArray(K.(x, x', θtru), bsn, bsn)
+    # D0    = Matrix.(fill(Eye{Float64}(blocksz),  nblocks))
+    # Dm1   = Matrix.(fill(Zeros{Float64}(blocksz,blocksz), nblocks-1))
+    # R′    = BlockBidiagonal(D0, Dm1,:L)
+    # M′ = BlockDiagonal(Matrix.(fill(Zeros{Float64}(blocksz,blocksz), nblocks)))
+    # view(M′, Block(1,1)) .= Σ[Block(1,1)]
+    # for i in 2:nblocks
+    #     bii  = Block(i,i)
+    #     bimi = Block(i,i-1)
+    #     A = - Σ[Block(i,i-1)] / Σ[Block(i-1,i-1)]
+    #     view(R′, Block(i,i-1))     .= A
+    #     view(M′, Block(i,i)) .= Σ[Block(i,i)] + A * Σ[Block(i,i-1)]'
+    # end 
+    # R = [R′[Block(i,i-1)] for i in 2:nblocks]
+    # M = [M′[Block(i,i)] for i in 1:nblocks]
+
+    V      = Vecchia(R, M, bsn)
+    iV     = InvVecchia(R, inv.(M), bsn)
+    iV′    = inv(V)
+
+    matRᴴ = VecchiaFactorization.Rᴴmat(V)
+    matR  = VecchiaFactorization.Rmat(V)
+
+    matV   = Matrix(V)
+    imatV  = Matrix(iV)
+    imatV′ = Matrix(iV′)
+    
+    #=
+    using PyPlot
+    matV    |> matshow; colorbar()
+    truV = K.(x, x', θtru) 
+    truV  |> matshow; colorbar()
+    (matV - truV)  |> matshow; colorbar()
+    
+    imatV   .|> abs .|> log |> matshow; colorbar()
+
+    matRᴴ   .|> abs .|> log |> matshow; colorbar()
+    matR    .|> abs .|> log |> matshow; colorbar()
+    =#
+    
+    v = randn(size(V,1))
+
+    v1 = V * v
+    v2 = iV * v
+
+    v1′ = matV * v
+    v2′ = imatV * v
+
+    @test v1 ≈ v1′ rtol=1e-5
+    @test v2 ≈ v2′ rtol=1e-5
+
+    #=
+    abs.(v1 .- v1′) |> plot
+    abs.(v2 .- v2′) |> plot
+    =# 
+
+    #= 
+    using BenchmarkTools
+    ## without threads ...
+    @benchmark V * v   # 48 μs
+    @benchmark iV * v  # 50 μs
+
+    @benchmark $(Matrix(matV)) * v    # 93 μs
+    @benchmark $(Matrix(imatV)) * v   # 90 μs
+
+    @benchmark $matV * v    # 108 μs
+    @benchmark $imatV * v   # 108 μs
+
+    (@belapsed $(Matrix(matV)) * v) / (@belapsed V * v)     # 1.7
+    Base.summarysize((Matrix(matV))) / Base.summarysize(V)  # 3.27
+    =#
+
 end
